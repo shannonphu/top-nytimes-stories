6f514ccd587401c4ebae95b4134c49ed3406cd04
shannonphu <shannonphu@sbcglobal.net>
2015-12-28 15:36:42 -0800

    made responsive

--- alt.js
+++ alt.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+	// Sidebar jQuery
+	$("#sidebar-button").click(function(){
+		$('.ui.labeled.icon.sidebar').sidebar('toggle');
+		$("#examples").height($(window).height() + 50);
+	});
+	$(this).click(function(){
+		$('.card').transition('fly right');
+		$('.card').transition('fly right');
+	});
+
 	// Deal with responsiveness
 	d3.select(window)
 	    .on("resize", setupForceLayout)
@@ -7,18 +17,69 @@ $(document).ready(function() {
 	// Set up d3 force layout
 	setupForceLayout();
 	
+	// var stories = mappedJSONToStory();
 });
 
+// Map JSON to story objects
+
+var BASE_URL_FRONT = "http://api.nytimes.com/svc/topstories/v1/";
+var BASE_URL_END = ".json?api-key=f0ea7f254bd61237ffaa7c92b9fa5979:11:73836972";
+
+function Story(story_title, abstract, content_url, byline, image_url, image_caption){
+	this.story_title = story_title;
+	this.abstract = abstract;
+	this.content_url = content_url;
+	this.byline = byline;
+	this.image_url = image_url;
+	this.image_caption = image_caption;
+};
+
+var dataOfSection = function(section) {
+	return BASE_URL_FRONT + section + BASE_URL_END;
+};
+
+var mappedJSONToStory = function() {
+	var stories = [];
+	
+	$.get( dataOfSection("world"), function(data) {
+		var results = data.results;
+		// Map each JSON object into Story object
+		results.forEach(function(story) {
+
+			// Iterate to find large image in array of images per story
+			var image_url;
+			var image_caption;
+			var imageLength = story["multimedia"].length;
+			for (var i = 0; i < imageLength; i++) {
+				var image = story.multimedia[i];
+				if (image.format == "superJumbo") {
+					image_url = image.url;
+					image_caption = image.caption;
+				};
+			};
+
+			// Construct new story for JSON object
+			var mappedStory = new Story(story.title, story.abstract, story.url, story.byline, image_url, image_caption);
+			stories.push(mappedStory);
+		});
+	  	console.log(stories);
+	});
+}
+
+// end mapping
+
 var setupForceLayout = function() {
-	var width = $(window).width() - $("#story-specific").width(),
+	var windowWidth = $(window).width();
+	// Determine width based on window width and device
+	var width = windowWidth < 640 ? windowWidth : windowWidth - $("#story-specific").width(),
 	    height = $(window).height(),
 	    padding = 1, // separation between same-color circles
 	    clusterPadding = 1, // separation between different-color circles
 	    minRadius = 30,
 	    maxRadius = 80;
 
-	var n = 50, // total number of circles
-	    m = 50; // number of distinct clusters
+	var n = 30, // total number of circles
+	    m = 30; // number of distinct clusters
 
 	var color = d3.scale.category10()
 	    .domain(d3.range(m));
@@ -28,7 +89,7 @@ var setupForceLayout = function() {
 
 	var nodes = d3.range(n).map(function() {
 	  var i = Math.floor(Math.random() * m),
-	      r = getRandomInt(minRadius, maxRadius),//Math.sqrt((i + 1) / m * - Math.log(Math.random())) * maxRadius,
+	      r = getRandomInt(minRadius, maxRadius),
 	      d = {cluster: i, radius: r};
 	  if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = d;
 	  	return d;
@@ -45,7 +106,13 @@ var setupForceLayout = function() {
 	d3.select("svg").remove();
 	var svg = d3.select("#examples").append("svg")
 	    .attr("width", width)
-	    .attr("height", height + 100);
+	    .attr("height", height + 50);
+
+	if (windowWidth < 640) {
+		$('main').css('top', $('.card').height() / 3);
+	} else {
+		$('main').css('top', -50);
+	};
 
 	// Define pattern fills for circles
 
@@ -77,6 +144,7 @@ var setupForceLayout = function() {
 		    .attr("fill", "url(#trial-pattern)")
 		    .call(force.drag);
 
+	// d3 force layout utility functions
 	function tick(e) {
 	  circle
 	      .each(cluster(10 * e.alpha * e.alpha))
@@ -131,6 +199,8 @@ var setupForceLayout = function() {
 	    });
 	  };
 	}
+
+	// end utility functions
 };
 
 // Returns a random integer between min (inclusive) and max (inclusive)
